f170b0a502ef93561749f5e8143c1554c3db886b
Kandyce <knhopkinson23@gmail.com>
2024-11-18 23:53:09 -0500

    added changes

---
+++ server/src/schemas/typeDefs.ts
@@ -0,0 +1,48 @@
+import { gql } from 'apollo-server-express';
+
+const typeDefs = gql`
+  type Book {
+    googleBookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Response {
+    user: User
+    message: String
+  }
+
+  type Query {
+    getUser: Response
+    getUserBooks: [Book]
+  }
+
+  type Mutation {
+    registerUser(username: String!, email: String!, password: String!): Response
+    loginUser(email: String!, password: String!): Response
+    logoutUser: Response
+    saveBook(googleBookId: String!, authors: [String]!, title: String!, description: String!, image: String!): Response
+    deleteBook(googleBookId: String!): Response
+  }
+
+  input BookInput {
+    googleBookId: String!
+    authors: [String]!
+    title: String!
+    description: String!
+    image: String!
+  }
+`;
+
+export default typeDefs;
